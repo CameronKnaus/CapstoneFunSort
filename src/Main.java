62a0cc02ad9a5e762157c0d729819f9f630ebb5d
CameronKnaus <cdknaus@gmail.com>
2020-04-19 17:03:35 -0400

    Refactor Main, remove errors, unused variables, fix Medium size issue

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,8 @@
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Cursor;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuItem;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -22,12 +13,6 @@ public class Main extends Application {
     // window size parameters
     private final int windowHeight = 720;
     private final int windowWidth = 1530;
-
-    // Number of colored bars to sort
-    private int numElements = 306;
-
-    // Main root of the scene as a Vertical Box
-    private VBox root;
 
     // The array to be sorted
     private Element[] elementList;
@@ -59,36 +44,27 @@ public class Main extends Application {
         Button shuffleButton = new Button("Shuffle");
 
         // Set the shuffle button action
-        shuffleButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                Sorter sorter = new Sorter(elementList, sortSection, 5);
-                sorter.shuffle();
-            }
+        shuffleButton.setOnAction(e -> {
+            Sorter sorter = new Sorter(elementList, sortSection, 5);
+            sorter.shuffle();
         });
 
         /* Bubble Sort Button */
         Button bubbleSort = new Button("Bubble Sort");
 
         // Set the bubble sort action
-        bubbleSort.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                Sorter sorter = new Sorter(elementList, sortSection, swapTime);
-                sorter.bubbleSort();
-            }
+        bubbleSort.setOnAction(e -> {
+            Sorter sorter = new Sorter(elementList, sortSection, swapTime);
+            sorter.bubbleSort();
         });
 
         /* Insertion Sort Button */
         Button insertionSort = new Button("Insertion Sort");
 
         // Set the insertion sort action
-        insertionSort.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                Sorter sorter = new Sorter(elementList, sortSection, swapTime);
-                sorter.insertionSort();
-            }
+        insertionSort.setOnAction(e -> {
+            Sorter sorter = new Sorter(elementList, sortSection, swapTime);
+            sorter.insertionSort();
         });
 
         /* Merge Sort Button */
@@ -113,16 +89,13 @@ public class Main extends Application {
         Button heapSort = new Button("Heap Sort");
 
         // Set the Quick sort action
-        heapSort.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                Sorter sorter = new Sorter(elementList, sortSection, swapTime);
-                sorter.heapSort();
-            }
+        heapSort.setOnAction(e -> {
+            Sorter sorter = new Sorter(elementList, sortSection, swapTime);
+            sorter.heapSort();
         });
 
         // Array Size Combo Box
-        final ComboBox arraySize = new ComboBox();
+        final ComboBox<String> arraySize = new ComboBox<>();
         arraySize.getItems().addAll(
                 "Very Small (10)",
                 "Small (30)",
@@ -133,38 +106,43 @@ public class Main extends Application {
         );
 
         // Add listener to update array size
-        arraySize.valueProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue ov, String t, String t1) {
-                // Set actions for each option
-                if(t1.equals("Very Small (10)"))
+        arraySize.valueProperty().addListener((ov, t, t1) -> {
+            // Set actions for each option
+            switch (t1) {
+                case "Very Small (10)":
                     elementList = ArrayInitializer.initialize(10, windowWidth, windowHeight);
-                else if(t1.equals("Small (30)"))
+                    break;
+                case "Small (30)":
                     elementList = ArrayInitializer.initialize(30, windowWidth, windowHeight);
-                else if(t1.equals("Medium (90)"))
+                    break;
+                case "Medium (90)":
                     elementList = ArrayInitializer.initialize(90, windowWidth, windowHeight);
-                else if(t1.equals("Large (300)"))
-                    elementList = ArrayInitializer.initialize(300, windowWidth, windowHeight);
-                else if(t1.equals("Very Large (765)"))
+                    break;
+                case "Large (300)":
+                    elementList = ArrayInitializer.initialize(306, windowWidth, windowHeight);
+                    break;
+                case "Very Large (765)":
                     elementList = ArrayInitializer.initialize(765, windowWidth, windowHeight);
-                else if(t1.equals("Max (1530)"))
+                    break;
+                case "Max (1530)":
                     elementList = ArrayInitializer.initialize(1530, windowWidth, windowHeight);
+                    break;
+            }
 
-                // Clear the current elements from the GUI
-                sortSection.getChildren().clear();
+            // Clear the current elements from the GUI
+            sortSection.getChildren().clear();
 
-                // Update the gui with the current elements
-                for (Element element : elementList) {
-                    sortSection.getChildren().add(element.getBody());
-                }
+            // Update the gui with the current elements
+            for (Element element : elementList) {
+                sortSection.getChildren().add(element.getBody());
             }
         });
         arraySize.setValue("Array Size");
 
         // Swap Speed Combo Box
-        final ComboBox swapSpeed = new ComboBox();
+        final ComboBox<String> swapSpeed = new ComboBox<>();
         swapSpeed.getItems().addAll(
-                "Very Fast (5ms)",
+                "Very Fast (7ms)",
                 "Fast (10ms)",
                 "Medium (25ms)",
                 "Slow (50ms)",
@@ -173,30 +151,35 @@ public class Main extends Application {
         );
 
         // Add listener to update array size
-        swapSpeed.valueProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue ov, String t, String t1) {
-                // Set actions for each option
-                if(t1.equals("Very Fast (5ms)"))
-                    swapTime = 5;
-                else if(t1.equals("Fast (10ms)"))
+        swapSpeed.valueProperty().addListener((ov, t, t1) -> {
+            // Set actions for each option
+            switch (t1) {
+                case "Very Fast (7ms)":
+                    swapTime = 7;
+                    break;
+                case "Fast (10ms)":
                     swapTime = 10;
-                else if(t1.equals("Medium (25ms)"))
+                    break;
+                case "Medium (25ms)":
                     swapTime = 25;
-                else if(t1.equals("Slow (50ms)"))
+                    break;
+                case "Slow (50ms)":
                     swapTime = 50;
-                else if(t1.equals("Very Slow (100ms)"))
+                    break;
+                case "Very Slow (100ms)":
                     swapTime = 100;
-                else if(t1.equals("Slowest (300ms)"))
+                    break;
+                case "Slowest (300ms)":
                     swapTime = 300;
-
+                    break;
             }
         });
 
         swapSpeed.setValue("Swap Time");
 
         // Create root of the scene (returns as main parent scene)
-        root = new VBox();
+        // Main root of the scene as a Vertical Box
+        VBox root = new VBox();
 
         // Create horizontal section for buttons
         HBox actionSection = new HBox(shuffleButton, bubbleSort,
@@ -224,6 +207,9 @@ public class Main extends Application {
     public void start(Stage stage) {
         // Name of the window
         stage.setTitle("Capstone FunSort");
+
+        // Number of elements to start with
+        int numElements = 306;
 
         // Create list of elements to sort
         elementList = ArrayInitializer.initialize(numElements, windowWidth, windowHeight);
